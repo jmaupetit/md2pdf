445912e1d54e538835addeb7027d6e85e3b9fc7a
Julien Maupetit <julien@maupetit.net>
2013-07-01 22:07:49 +0200

    Add basic CLI

--- md2pdf.py
+++ md2pdf.py
@@ -1,25 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""md2pdf - Markdown to PDF conversion tool.
+
+Usage: md2pdf.py [options] INPUT.MD OUTPUT.PDF
+
+Options:
+    --css=STYLE.CSS
+"""
+
 import sys
 
+from docopt import docopt
 from markdown2 import markdown_path
 from weasyprint import HTML, CSS
 
 
+__title__ = 'md2pdf'
+__version__ = '0.1'
+__author__ = 'Julien Maupetit'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2013 Julien Maupetit'
+
+
 def main(argv=None):
 
+    # Parse command line arguments
+    arguments = docopt(
+        __doc__,
+        version='md2pdf %s' % __version__)
+
+    # Paths
+    md_file_path = arguments.get('INPUT.MD')
+    pdf_file_path = arguments.get('OUTPUT.PDF')
+    css_file_path = arguments.get('STYLE.CSS', None)
+
     # Convert markdown to html
-    html = markdown_path('markdown-css-themes/sample.md')
+    raw_html = markdown_path(md_file_path)
 
     # Weasyprint HTML object
-    html = HTML(string=html)
+    html = HTML(string=raw_html)
 
     # Get styles
-    css = CSS(filename='markdown-css-themes/foghorn.css')
+    css = []
+    if css_file_path:
+        css.append(CSS(filename=css_file_path))
 
     # Generate PDF
-    html.write_pdf('test.pdf', stylesheets=[css])
+    html.write_pdf(pdf_file_path, stylesheets=css)
 
     return 1
 
