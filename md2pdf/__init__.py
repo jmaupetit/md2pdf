73bf5c47d3496e028d47149eb79ea3b16da2a93e
Julien Maupetit <julien@maupetit.net>
2021-01-21 21:03:22 +0100

    Bump release to 0.5
    - Add markdown table support
    - Improve python 3 support
    - Improve Docker support

--- md2pdf/__init__.py
+++ md2pdf/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __title__ = 'md2pdf'
-__version__ = '0.4'
+__version__ = '0.5'
 __author__ = 'Julien Maupetit'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2013-2017 Julien Maupetit'
+__copyright__ = 'Copyright 2013-2021 Julien Maupetit'
 
 from md2pdf.core import md2pdf  # noqa
