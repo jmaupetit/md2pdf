fb01e8cd88e97348127ce4cc6c7d4e4f3ed614eb
Julien Maupetit <julien@maupetit.net>
2023-04-12 09:53:03 +0200

    Fix installation issue with recent pip releases
    Fix #48

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@
 md2pdf - setup file
 """
 
-import md2pdf
+from importlib.metadata import version
 
 from setuptools import setup, find_packages
 
 
-def parse_requirements(requirements, ignore=('setuptools',)):
+def parse_requirements(requirements, ignore=("setuptools",)):
     """
     Read dependencies from requirements file (with version numbers if any)
 
@@ -22,10 +22,10 @@ def parse_requirements(requirements, ignore=('setuptools',)):
         packages = set()
         for line in f:
             line = line.strip()
-            if line.startswith(('#', '-r', '--')):
+            if line.startswith(("#", "-r", "--")):
                 continue
-            if '#egg=' in line:
-                line = line.split('#egg=')[1]
+            if "#egg=" in line:
+                line = line.split("#egg=")[1]
             pkg = line.strip()
             if pkg not in ignore:
                 packages.add(pkg)
@@ -33,36 +33,36 @@ def parse_requirements(requirements, ignore=('setuptools',)):
 
 
 setup(
-    name='md2pdf',
-    version=md2pdf.__version__,
+    name="md2pdf",
+    version=version("md2pdf"),
     packages=find_packages(),
-    install_requires=parse_requirements('requirements.txt'),
-    setup_requires=['pytest-runner', ],
-    tests_require=parse_requirements('requirements-dev.txt'),
-    author='Julien Maupetit',
-    author_email='julien@maupetit.net',
-    description='md2pdf, a Markdown to PDF conversion tool',
-    license='MIT',
-    keywords='markdown converter css pdf',
+    install_requires=parse_requirements("requirements.txt"),
+    setup_requires=[
+        "pytest-runner",
+    ],
+    tests_require=parse_requirements("requirements-dev.txt"),
+    author="Julien Maupetit",
+    author_email="julien@maupetit.net",
+    description="md2pdf, a Markdown to PDF conversion tool",
+    license="MIT",
+    keywords="markdown converter css pdf",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Customer Service',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'Intended Audience :: Other Audience',
-        'License :: OSI Approved :: MIT License',
-        'Environment :: Console',
-        'Operating System :: MacOS',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Office/Business',
-        'Topic :: Utilities',
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Customer Service",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Other Audience",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Office/Business",
+        "Topic :: Utilities",
     ],
 )
