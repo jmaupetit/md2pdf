5aaf183d9a4c546362251520bca5e3fdc5d9fc72
Julien Maupetit <julien@maupetit.net>
2024-01-17 23:23:01 +0100

    Remove explicit version management in the package sources
    Use package metadata instead.

--- md2pdf/__init__.py
+++ md2pdf/__init__.py
@@ -1,8 +1,3 @@
-# -*- coding: utf-8 -*-
-__title__ = "md2pdf"
-__author__ = "Julien Maupetit"
-__license__ = "MIT"
-__copyright__ = "Copyright 2013-2023 Julien Maupetit"
+"""md2pdf root module."""
 
 from .core import md2pdf  # noqa
-from .version import __version__  # noqa
