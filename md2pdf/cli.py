5e158000cbf3f98cead282b2b53b86550430070f
Julien Maupetit <julien@maupetit.net>
2024-01-22 23:30:45 +0100

    Allow to configure Markdown extras using the command line

--- md2pdf/cli.py
+++ md2pdf/cli.py
@@ -11,12 +11,14 @@ from md2pdf.core import md2pdf
 @click.argument("md", type=click.Path(exists=True))
 @click.argument("pdf", type=click.Path())
 @click.option("--css", "css", type=click.Path(exists=True))
+@click.option("--extras", "-e", "extras", multiple=True, type=str)
 @click.version_option(version=version("md2pdf"))
-def cli(md, pdf, css):
+def cli(md, pdf, css, extras):
     """md2pdf command line tool."""
     md2pdf(
         pdf,
         md=md,
         css=css,
         base_url=Path.cwd(),
+        extras=extras
     )
