ae2b84b875f263b26c11caa9b80e0a124fc16c3d
Julien Maupetit <julien@maupetit.net>
2024-01-23 22:30:08 +0100

    Add md template support using Jinja and frontmatter

--- md2pdf/cli.py
+++ md2pdf/cli.py
@@ -1,10 +1,14 @@
 """md2pdf CLI."""
+import logging
 from importlib.metadata import version
 from pathlib import Path
+from typing import Optional
 
 import click
 
 from md2pdf.core import md2pdf
+
+logger = logging.getLogger(__name__)
 
 
 @click.command(name="md2pdf")
@@ -13,12 +17,8 @@ from md2pdf.core import md2pdf
 @click.option("--css", "css", type=click.Path(exists=True))
 @click.option("--extras", "-e", "extras", multiple=True, type=str)
 @click.version_option(version=version("md2pdf"))
-def cli(md, pdf, css, extras):
+def cli(md: Path, pdf: Path, css: Optional[Path], extras: list):
     """md2pdf command line tool."""
-    md2pdf(
-        pdf,
-        md=md,
-        css=css,
-        base_url=Path.cwd(),
-        extras=extras
-    )
+    if css is not None:
+        css = Path(css)
+    md2pdf(Path(pdf), md=Path(md), css=css, base_url=Path.cwd(), extras=extras)
--- md2pdf/core.py
+++ md2pdf/core.py
@@ -1,12 +1,17 @@
 """md2pdf core module."""
+import logging
 from pathlib import Path
 from typing import List, Optional
 
-from markdown2 import markdown, markdown_path
+import frontmatter
+from jinja2 import Template
+from markdown2 import markdown
 from weasyprint import CSS, HTML
 
 from .conf import MARKDOWN_EXTRAS
 from .exceptions import ValidationError
+
+logger = logging.getLogger(__name__)
 
 
 def md2pdf(
@@ -33,16 +38,26 @@ def md2pdf(
     Raises:
         ValidationError: if md_content and md_file_path are empty.
     """
-    # Convert markdown to html
-    raw_html: str = ""
-
     if md:
-        raw_html = markdown_path(md, extras=extras)
-    elif raw:
-        raw_html = markdown(raw, extras=extras)
+        logger.debug("Reading markdown content from file %s", md)
+        raw = md.read_text()
 
-    if not len(raw_html):
+    if raw is None or not len(raw):
         raise ValidationError("Input markdown seems empty")
+
+    context: dict = {}
+    # Check if markdown file is a template
+    if frontmatter.checks(raw):
+        logger.info("Markdown input file contains frontmatter header")
+
+        # Get context and the template
+        context, template = frontmatter.parse(raw)
+        logger.debug("Frontmatter context %s", context)
+
+        # Render the template
+        raw = Template(template).render(context)
+
+    raw_html = markdown(raw, extras=extras)
 
     # Weasyprint HTML object
     if base_url is None:
