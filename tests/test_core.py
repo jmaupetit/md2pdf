bca8f35cb68ad0af814891dea25d388c2d3f0ce6
Julien Maupetit <julien@maupetit.net>
2026-01-12 21:27:48 +0100

    ✅(core) add tests for jinja templates
    Add more test for Jinja template rendering using a context from
    frontmatter or a dictionnary.

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,6 +1,7 @@
 """md2pdf tests for the core module."""
 
 import pytest
+from pypdf import PdfReader
 
 from md2pdf import md2pdf
 from md2pdf.exceptions import ValidationError
@@ -22,6 +23,75 @@ def test_generate_pdf_from_raw_markdown():
 
     md2pdf(OUTPUT_PDF, raw="# hi there!")
     assert OUTPUT_PDF.exists()
+
+    # Content
+    reader = PdfReader(OUTPUT_PDF)
+    assert reader.pages[0].extract_text() == "hi there!"
+
+
+def test_generate_pdf_with_jinja_frontmatter_input():
+    """Generate a PDF using a frontmatter header source and a Jinja template."""
+    assert not OUTPUT_PDF.exists()
+
+    raw = "\n".join(
+        [
+            "---",
+            "name: John",
+            "id: 2",
+            "---",
+            "Hey {{ name }} 👋\n",
+            "ID: {{ id }}",
+        ]
+    )
+    md2pdf(OUTPUT_PDF, raw=raw)
+    assert OUTPUT_PDF.exists()
+
+    # Content
+    reader = PdfReader(OUTPUT_PDF)
+    assert reader.pages[0].extract_text() == "Hey John 👋\nID: 2"
+
+
+def test_generate_pdf_with_jinja_context_input():
+    """Generate a PDF using an input context and Jinja template."""
+    assert not OUTPUT_PDF.exists()
+
+    raw = "\n".join(
+        [
+            "Hey {{ name }} 👋\n",
+            "ID: {{ id }}",
+        ]
+    )
+    md2pdf(OUTPUT_PDF, raw=raw, context={"name": "John", "id": 2})
+    assert OUTPUT_PDF.exists()
+
+    # Content
+    reader = PdfReader(OUTPUT_PDF)
+    assert reader.pages[0].extract_text() == "Hey John 👋\nID: 2"
+
+
+def test_generate_pdf_with_jinja_frontmatter_and_context_input():
+    """Generate a PDF using a frontmatter header source, a context and a template.
+
+    Frontmatter value takes precedence over direct context.
+    """
+    assert not OUTPUT_PDF.exists()
+
+    raw = "\n".join(
+        [
+            "---",
+            "name: Jane",
+            "id: 2",
+            "---",
+            "Hey {{ name }} 👋\n",
+            "ID: {{ id }}",
+        ]
+    )
+    md2pdf(OUTPUT_PDF, raw=raw, context={"name": "John"})
+    assert OUTPUT_PDF.exists()
+
+    # Content
+    reader = PdfReader(OUTPUT_PDF)
+    assert reader.pages[0].extract_text() == "Hey Jane 👋\nID: 2"
 
 
 def test_raises_a_validation_error_when_generated_html_is_empty():
