2ffac7138534246d199438253e10aea8573553f3
Julien Maupetit <julien@maupetit.net>
2026-01-25 21:47:15 +0100

    ✨(cli) add `--watch` option
    When working on a markdown document or a companion stylesheet, it's
    usefull to automatically re-render the corresponding PDF file upon
    changes.

--- src/md2pdf/cli.py
+++ src/md2pdf/cli.py
@@ -3,10 +3,11 @@
 import json
 import logging
 from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from importlib.metadata import version as metadata_version
 from pathlib import Path
 from time import time
-from typing import Annotated, Optional
+from typing import Annotated, List, Optional
 
 import typer
 from rich.console import Console
@@ -17,7 +18,9 @@ from rich.progress import (
     TextColumn,
     TimeElapsedColumn,
 )
+from watchfiles import watch as wf_watch
 
+from .conf import CLI_WATCH_FORCE_POOLING
 from .core import md2pdf
 from .exceptions import ValidationError
 
@@ -36,6 +39,84 @@ def parse_config(config: str) -> dict:
             "Invalid input configuration string (should be valid JSON)"
         ) from err
     return parsed
+
+
+def _run_with_progress(
+    progress: Progress,
+    task: TaskID,
+    md_: Path,
+    pdf: Path,
+    css: Optional[Path] = None,
+    extras: Optional[list[str]] = None,
+    extras_config: Optional[dict] = None,
+):
+    """A wrapper for md2pdf thread execution with task progress update."""
+    progress.start_task(task)
+    md2pdf(
+        pdf,
+        md=md_,
+        css=css,
+        base_url=Path.cwd(),
+        extras=extras if extras else None,
+        extras_config=extras_config,
+    )
+    progress.update(task, completed=True)
+
+
+def _start_threads(
+    progress: Progress,
+    workers: int,
+    md: List[Path],
+    pdf: Optional[Path] = None,
+    css: Optional[Path] = None,
+    extras: Optional[list[str]] = None,
+    extras_config: Optional[dict] = None,
+):
+    """Run convertion in threads with progress."""
+    started_at = time()
+    tasks = []
+    with progress:
+        with ThreadPoolExecutor(max_workers=workers) as pool:
+            for md_ in md:
+                pdf_ = md_.with_suffix(".pdf") if pdf is None else pdf
+                task = progress.add_task(
+                    "convert", md=md_, pdf=pdf_, total=1, start=False
+                )
+                pool.submit(
+                    _run_with_progress,
+                    progress,
+                    task,
+                    md_,
+                    pdf_,
+                    css,
+                    extras,
+                    extras_config,
+                )
+                tasks.append(task)
+
+    console.print(f"🚀 Output files generated in [blue]{(time() - started_at):.3f}s[/]")
+
+    # Clean tasks
+    for task in tasks:
+        progress.remove_task(task)
+
+
+def _get_progress(console: Console) -> Progress:
+    """Get rich progress component."""
+    return Progress(
+        SpinnerColumn(finished_text="✅"),
+        TextColumn(
+            "Converting: [blue]{task.fields[md]}[/blue] "
+            "→ [green]{task.fields[pdf]}[/green]"
+        ),
+        "·",
+        TimeElapsedColumn(),
+        console=console,
+    )
+
+
+def watcher_callback():
+    """Dummy watcher callback used for testing."""
 
 
 @cli.command(no_args_is_help=True)
@@ -76,6 +157,12 @@ def main(
             help="Markdown extensions configuration (as a JSON string).",
         ),
     ] = None,
+    watch: Annotated[
+        bool,
+        typer.Option(
+            "--watch", "-w", help="Automatically render PDF upon input file(s) changes."
+        ),
+    ] = False,
     workers: Annotated[
         int,
         typer.Option("--workers", "-W", help="Number of parallel workers to start."),
@@ -112,39 +199,30 @@ def main(
         extras_config = parse_config(config)
         console.print(f"🔧 Configuration: [blue]{extras_config}[/blue]")
 
-    progress = Progress(
-        SpinnerColumn(finished_text="✅"),
-        TextColumn(
-            "Converting: [blue]{task.fields[md]}[/blue] "
-            "→ [green]{task.fields[pdf]}[/green]"
-        ),
-        "·",
-        TimeElapsedColumn(),
-        console=console,
+    # Run rendering and exit (if watch is not active)
+    _start_threads(_get_progress(console), workers, md, pdf, css, extras, extras_config)
+    if not watch:
+        raise typer.Exit()
+
+    # Watch changes in CSS and markdown files
+    paths_to_watch = ([css] if css else []) + md
+    console.print(
+        f"👀 Looking for changes in: {[str(p) for p in paths_to_watch]} "
+        "(CTRL+C to quit)"
     )
+    for changes in wf_watch(
+        *paths_to_watch,
+        force_polling=CLI_WATCH_FORCE_POOLING,
+    ):
+        changed_files = {Path(change[1]) for change in changes}
+        console.rule(str(datetime.now()), align="right")
+        console.print(f"⚡️ Detected changes in: {list(map(str, changed_files))}")
+        changed_md = list(changed_files & set(md))
+        if css in changed_files:
+            changed_md = md
 
-    def wrapper(task: TaskID, md_: Path, pdf: Path):
-        """A wrapper for md2pdf thread execution with task progress update."""
-        progress.start_task(task)
-        md2pdf(
-            pdf,
-            md=md_,
-            css=css,
-            base_url=Path.cwd(),
-            extras=extras if extras else None,
-            extras_config=extras_config,
+        _start_threads(
+            _get_progress(console), workers, changed_md, pdf, css, extras, extras_config
         )
-        progress.update(task, completed=True)
 
-    started_at = time()
-    with progress:
-        with ThreadPoolExecutor(max_workers=workers) as pool:
-            for md_ in md:
-                if pdf is None:
-                    pdf = md_.with_suffix(".pdf")
-                task = progress.add_task(
-                    "convert", md=md_, pdf=pdf, total=1, start=False
-                )
-                pool.submit(wrapper, task, md_, pdf)
-
-    console.print(f"🚀 Output files generated in [blue]{(time() - started_at):.3f}s[/]")
+        watcher_callback()
