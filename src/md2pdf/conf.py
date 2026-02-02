f0f54e3fbb265982918378503f62c81b489d74e4
Julien Maupetit <julien@maupetit.net>
2026-02-02 18:40:54 +0100

    ✨(core) add input template support
    We are now able to encapsulate input markdown content in a base
    template. Default template only defines base HTML file structure and
    puts input markdown content in the body of the document.

--- src/md2pdf/conf.py
+++ src/md2pdf/conf.py
@@ -1,5 +1,7 @@
 """Configuration for md2pdf."""
 
+from pathlib import Path
+
 MARKDOWN_BASE_EXTENSIONS: list = [
     "markdown.extensions.footnotes",
     "markdown.extensions.tables",
@@ -16,3 +18,6 @@ MARKDOWN_BASE_EXTENSIONS: list = [
 
 # FIXME: shouldn't be required for Linux
 CLI_WATCH_FORCE_POOLING = True
+
+# Templates
+BASE_TEMPLATE = Path(__file__).parent / Path("./templates/base.html.j2")
--- src/md2pdf/core.py
+++ src/md2pdf/core.py
@@ -9,7 +9,7 @@ from jinja2 import Template
 from markdown import markdown
 from weasyprint import CSS, HTML
 
-from .conf import MARKDOWN_BASE_EXTENSIONS
+from .conf import BASE_TEMPLATE, MARKDOWN_BASE_EXTENSIONS
 from .exceptions import ValidationError
 
 logger = logging.getLogger(__name__)
@@ -24,6 +24,7 @@ def md2pdf(
     extras: Optional[List[str]] = None,
     extras_config: Optional[dict] = None,
     context: Optional[dict] = None,
+    template: Path = BASE_TEMPLATE,
 ):
     """Converts input markdown to styled HTML and renders it to a PDF file.
 
@@ -36,6 +37,7 @@ def md2pdf(
         extras: supplementary markdown extensions to activate
         extras_config: a configuration dictionnary for active markdown extensions
         context: input context to use for jinja template rendering
+        template: base document template
 
     Returns:
         None
@@ -49,34 +51,45 @@ def md2pdf(
     # Merge base extensions with extras extensions
     extras = extras if extras and len(extras) else []
 
+    input_md: str | None = raw
     if md:
         logger.debug("Reading markdown content from file %s", md)
-        raw = md.read_text()
+        input_md = md.read_text()
 
-    if raw is None or not len(raw):
+    if input_md is None or not len(input_md):
         raise ValidationError(
             "No markdown content to process (empty file or raw string)"
         )
 
-    # Check if markdown file is a template
-    if frontmatter.checks(raw):
+    # Check if markdown file contains a frontmatter header extract context from it
+    if frontmatter.checks(input_md):
         logger.info("Markdown input file contains frontmatter header")
 
         # Get context and the template
-        ftmt_context, raw = frontmatter.parse(raw)
+        ftmt_context, input_md = frontmatter.parse(input_md)
         logger.debug("Frontmatter context %s", context)
         context.update(ftmt_context)
 
-    # Render the template
-    raw = Template(raw).render(context)
+    # Render (markdown) jinja template
+    input_md = Template(input_md).render(context)
 
-    extensions = MARKDOWN_BASE_EXTENSIONS + extras
-    raw_html = markdown(raw, extensions=extensions, extension_configs=extras_config)
+    # Render markdown to HTML
+    extensions: List[str] = MARKDOWN_BASE_EXTENSIONS + extras
+    inner_html = markdown(
+        input_md, extensions=extensions, extension_configs=extras_config
+    )
+
+    # Render the template
+    full_context = {"content": inner_html, "pdf": {"title": "Generated with md2pdf"}}
+    full_context.update(context)
+
+    input_html = Template(template.read_text()).render(full_context)
+    logger.debug("Input HTML that will be sent to Weasyprint: {input_html}")
 
     # Weasyprint HTML object
     if base_url is None:
         base_url = Path.cwd()
-    html: HTML = HTML(string=raw_html, base_url=str(base_url))
+    html: HTML = HTML(string=input_html, base_url=str(base_url))
 
     # Get styles
     styles: list = []
